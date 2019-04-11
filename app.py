48b91635044c7ef853d77f64f59fe31bdb47ab4e
Samantha Tuapen <samtuapen@utexas.edu>
2019-04-10 19:09:16 -0500

    changes

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template, request
+
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
-app.run(debug = True)
+def index():
+	return render_template('index.html')
+
+if __name__ == '__main__':
+	app.run(debug = True)
+
+
