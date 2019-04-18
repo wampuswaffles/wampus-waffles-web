86a866e47c8105edf4f2389aae781432287b051e
Samantha Tuapen <samtuapen@utexas.edu>
2019-04-17 19:16:21 -0500

    fixed all links, changed some text, and created temp database

--- app.py
+++ app.py
@@ -6,6 +6,13 @@ app = Flask(__name__)
 def index():
 	return render_template('index.html')
 
+@app.route('/', methods=['GET', 'POST'])
+def foo():
+	with open("database.txt", "a") as myfile:
+		myfile.write(request.form['message']+"\n")
+	'''execute whatever code you want when the button gets clicked here'''
+	return index()#"hello world"
+
 if __name__ == '__main__':
 	app.run(debug = True)
 
